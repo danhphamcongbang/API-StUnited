b4f3a40fc565249d51a792393d6fc8060e1c6e37
danhphamcongbang <bang.danh_dp04@devplus.edu.vn>
2022-12-13 01:55:37 +0700

    update API

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const FE02TypicalRouter = require("./routes/FE02TypicalRoute/FE02Typical")
 const Fe07Div01Router =require("./routes/FE07STSOFTWARERoute/FE07Div1Router")
 const Fe07Div02Router =require("./routes/FE07STSOFTWARERoute/FE07Div2Router")
 dotenv.config()
+mongoose.set('strictQuery', true);
 
 mongoose.connect((process.env.MONGODB_URL),() => {
     console.log("connected to MongoDB Done")
